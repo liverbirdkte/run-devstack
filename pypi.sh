4648a25e39ee44b16e646c62414045f927fc1cb4
Yongfeng Du <dolpher.du@intel.com>
2015-12-11 08:08:46 +0000

    Fix the pypi setting

--- pypi.sh
+++ pypi.sh
@@ -5,8 +5,11 @@ PIP_CONF=/home/ubuntu/.pip
 mkdir $PIP_CONF
 cat >> $PIP_CONF/pip.conf << EOF
 [global]
-#index-url = http://linux-ftp.jf.intel.com/pub/mirrors/pypi/web/simple
-#trusted-host = linux-ftp.jf.intel.com
 index-url = http://shtaurus.sh.intel.com/pypi/simple
 trusted-host = shtaurus.sh.intel.com
+#index-url = http://linux-ftp.jf.intel.com/pub/mirrors/pypi/web/simple
+#trusted-host = linux-ftp.jf.intel.com
 EOF
+
+# /root also need to set the pypi mirror
+sudo cp -rf $PIP_CONF /root
