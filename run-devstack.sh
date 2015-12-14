153769f70173ea6e82dff845fa6bca0134f1df07
Yongfeng Du <dolpher.du@intel.com>
2015-12-14 07:01:38 +0000

    Fix the gen-localconf.sh path problem

--- run-devstack.sh
+++ run-devstack.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -x
+
+OLD_DIR=$PWD
 # Clone the latest devstack
 cd ~
 git clone /opt/git/openstack-dev/devstack.git
 
 cd ~/devstack
 # generate local.conf
-./gen-localconf.sh
+$OLD_DIR/gen-localconf.sh
 
 ./stack.sh
