ae9093745f4d478f2b60c38a96905d50b3f47352
Yongfeng Du <dolpher.du@intel.com>
2015-12-17 15:11:32 +0800

    Add LM support
    Change-Id: I5e30906d874154da3ff7b6be6e9152fed6d3c79f

---
+++ lm-prep.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Prepare the Live Migration requirements
+# instances nfs share
+MY_IP=`ifconfig eth0| awk '/inet addr/{print substr($2,6)}'`
+# Generate the local.conf file
+NFS_PATH=/opt/stack/data/nova/instances
+if [ $MY_IP == $CTRL_IP ]
+then
+    sudo apt install -y nfs-kernel-server
+    mkdir -p $NFS_PATH
+    grep $NFS_PATH /etc/exports
+    if [ $? -ne 0 ]
+    then echo "$NFS_PATH       *(rw,sync,no_subtree_check)" | sudo -E tee -a /etc/exports
+    fi
+    sudo service nfs-kernel-server restart
+else
+    mkdir -p $NFS_PATH
+    sudo mount -t nfs $CTRL_IP:$NFS_PATH $NFS_PATH
+    grep $NFS_PATH /etc/fstab
+    if [ $? -ne 0 ]
+    echo "$CTRL_IP:$NFS_PATH $NFS_PATH  nfs defaults 0 0" | sudo -E tee -a /etc/fstab
+    fi
+fi
+
+# sshkey
+#ssh-keygen -N "" -f ~/.ssh/id_rsa
+#ssh-copy-id
+#ssh-keyscan -H 192.168.2.68 >> ~/.ssh/known_hosts
+cp id_rsa.lm ~/.ssh/id_rsa
+cat id_rsa.lm.pub >> ~/.ssh/authorized_keys
+echo "    StrictHostKeyChecking no" |sudo -E tee -a /etc/ssh/ssh_config
+echo "    UserKnownHostsFile=/dev/null" |sudo -E tee -a /etc/ssh/ssh_config
+
+# libvirt config
+sudo sed -i "s|libvirtd_opts=\"-d\"|libvirtd_opts=\"-l -d\"|" /etc/default/libvirt-bin
+sudo sed -i "s|#listen_tls = 0|listen_tls = 0|" /etc/libvirt/libvirtd.conf
+sudo sed -i "s|#listen_tcp = 1|listen_tcp = 1|" /etc/libvirt/libvirtd.conf
+sudo sed -i "s|#auth_tcp = \"sasl\"|auth_tcp = \"none\"|" /etc/libvirt/libvirtd.conf
+
+sudo service libvirt-bin restart
+
+# nova cpu mode
--- prep.sh
+++ prep.sh
@@ -5,12 +5,7 @@ sudo apt install -y nfs-common git
 sudo mkdir /opt/git
 sudo mount -t nfs 192.168.0.2:/data/git /opt/git
 
-# instances nfs share
-
-# sshkey
-#ssh-keygen -N "" -f ~/.ssh/id_rsa
-#ssh-copy-id
-#ssh-keyscan -H 192.168.2.68 >> ~/.ssh/known_hosts
-# libvirt config
-
-# nova cpu mode
+if [ $LM_ENABLED = "True" ]
+then
+./lm-prep.sh
+fi
